ebcdfaeef4089680f9b1f2f0c8c4dfe636ce77eb
TMTron <martin.trummer@tmtron.com>
2017-11-13 12:56:24 +0100

    solved T05b.03 PolishAsyncTask

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -16,6 +16,7 @@
 package com.example.android.asynctaskloader;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
@@ -165,7 +166,8 @@ public class MainActivity extends AppCompatActivity implements
     public Loader<String> onCreateLoader(int id, final Bundle args) {
         return new AsyncTaskLoader<String>(this) {
 
-            // TODO (1) Create a String member variable called mGithubJson that will store the raw JSON
+            // DONE (1) Create a String member variable called mGithubJson that will store the raw JSON
+            private String mGithubJson;
 
             @Override
             protected void onStartLoading() {
@@ -181,8 +183,12 @@ public class MainActivity extends AppCompatActivity implements
                  */
                 mLoadingIndicator.setVisibility(View.VISIBLE);
 
-                // TODO (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
-                forceLoad();
+                // DONE (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
+                if (mGithubJson != null) {
+                    deliverResult(mGithubJson);
+                } else {
+                    forceLoad();
+                }
             }
 
             @Override
@@ -207,8 +213,14 @@ public class MainActivity extends AppCompatActivity implements
                 }
             }
 
-            // TODO (3) Override deliverResult and store the data in mGithubJson
-            // TODO (4) Call super.deliverResult after storing the data
+            // DONE (3) Override deliverResult and store the data in mGithubJson
+            @Override
+            public void deliverResult(@Nullable String data) {
+                // DONE (4) Call super.deliverResult after storing the data
+                mGithubJson = data;
+                super.deliverResult(data);
+            }
+
         };
     }
 
