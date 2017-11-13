acd785ffad4b02dd17fa4e07a72f0eacd47f8ff7
TMTron <martin.trummer@tmtron.com>
2017-11-13 11:24:24 +0100

    solved T05a.03

--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -39,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    // DOINE (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    private static final List<String> mLifecycleCallbacks = new ArrayList<>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -71,9 +76,15 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        // DONE (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        for (ListIterator<String> i = mLifecycleCallbacks.listIterator(mLifecycleCallbacks.size());
+             i.hasPrevious();) {
+            final String lifecycleEvent = i.previous();
+            appendToLifecycleDisplay(lifecycleEvent);
+        }
 
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
+        // DONE (5) Clear mLifecycleCallbacks after iterating through it
+        mLifecycleCallbacks.clear();
 
         logAndAppend(ON_CREATE);
     }
@@ -133,7 +144,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        // DONE (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        mLifecycleCallbacks.add(0, ON_STOP);
 
         logAndAppend(ON_STOP);
     }
@@ -160,7 +172,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        // DONE (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        mLifecycleCallbacks.add(0, ON_DESTROY);
 
         logAndAppend(ON_DESTROY);
     }
@@ -183,6 +196,17 @@ public class MainActivity extends AppCompatActivity {
     private void logAndAppend(String lifecycleEvent) {
         Log.d(TAG, "Lifecycle Event: " + lifecycleEvent);
 
+        appendToLifecycleDisplay(lifecycleEvent);
+    }
+
+    /**
+     * Appends the lifecycle method name to the TextView so that you can
+     * view the series of method callbacks that are called both from the app and from within
+     * Android Studio's Logcat.
+     *
+     * @param lifecycleEvent The name of the event to be logged.
+     */
+    private void appendToLifecycleDisplay(String lifecycleEvent) {
         mLifecycleDisplay.append(lifecycleEvent + "\n");
     }
 
